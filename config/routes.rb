9f4fb565a05621b1054c1c8fcefd769da23d7057
shivasaki <shivasaki0329@gmail.com>
2024-02-12 22:18:16 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/delete'
+
+  resources :guchis
+  root 'guchis#index'
+  get 'guchis/index'
+  get 'guchis/new'
+  get 'guchis/show'
+  get 'guchis/edit'
+  get 'guchis/delete'
 end
